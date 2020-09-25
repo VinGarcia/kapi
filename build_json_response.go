6d0dee60c7d73b0d05656c70c21264d9caa459f9
Vinícius Garcia <vingarcia00@gmail.com>
2020-09-25 10:26:56 -0300

    Try to improve rendering again

--- build_json_response.go
+++ build_json_response.go
@@ -8,13 +8,10 @@ import (
 	routing "github.com/jackwhelpton/fasthttp-routing/v2"
 )
 
-// """
 // BuildJSONResponse is a oneliner for 3 common actions:
-//
-// - Marshal a struct or map into JSON
-// - Set the response status
-// - Set the content-type to `application/json`.
-// """
+// (1) Marshal a struct or map into JSON;
+// (2) Set the response status;
+// (3) Set the content-type to `application/json`.
 func BuildJSONResponse(ctx *routing.Context, statusCode int, body interface{}) error {
 	rawJSON, err := json.Marshal(body)
 	if err != nil {
